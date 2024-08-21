8200740fa8c8b6e55a3996be18f51d27aeaec765
codebruno <b.webdevs@gmail.com>
2024-08-21 19:41:48 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Scroll to top functionality
+    document.getElementById('scrollToTop').addEventListener('click', function () {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+});
